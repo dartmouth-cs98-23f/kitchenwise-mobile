461525441b85199eca5bbd3a3460caa9d44eadc6
Stefel Smith <stefelsmith@gmail.com>
2024-02-22 17:06:09 -0500

    Fixed merge issues

--- navigation/ProfilePage.js
+++ navigation/ProfilePage.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity} from "react-native";
 import Navbar from "./Navbar";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import themeStyles from "../styles";
+import InventoryContext from "../context/inventory-context";
+import InventoryPill from "../components/profile_components/InventoryPill";
+import AddInventoryPill from "../components/profile_components/AddInventoryPill";
 
 const ProfilePage = () => {
   const navigation = useNavigation();
@@ -12,12 +14,9 @@ const ProfilePage = () => {
   const navigateToInventoryStatisticsPage = () => {
     navigation.navigate('InventoryStatistics');
 }
-import themeStyles from "../styles";
-import InventoryContext from "../context/inventory-context";
-import InventoryPill from "../components/profile_components/InventoryPill";
-import AddInventoryPill from "../components/profile_components/AddInventoryPill";
 
-const ProfilePage = () => {
+
+
   const { userInventories } = useContext(InventoryContext);
   return (
     <>
