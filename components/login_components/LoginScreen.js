14b8b8de2b8066bb15ca8bb6816893df111a4d0a
syedh482 <syed.i.hussaini.24@dartmouth.edu>
2024-03-03 15:03:24 -0500

    login ui & setup for auth

--- components/login_components/LoginScreen.js
+++ components/login_components/LoginScreen.js
@@ -1,170 +1,160 @@
-import React, { useCallback, useContext, useState } from "react";
-import { View, Modal, StyleSheet, Text, TextInput } from "react-native";
-import { Input } from "../form_components";
-import LoginButton from "./LoginButton";
-import UserContext from "../../context/user-context";
-
-const DEFAULT_USER_ID = "653997da2d9889247c37976e";
+import React, { useState, useContext } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import UserContext from '../../context/user-context';
 
 const LoginScreen = ({ navigation }) => {
-  const [isNewAccount, setIsNewAccount] = useState(false);
-  const [isLogin, setIsLogin] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const { userId, setUserId } = useContext(UserContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { setUserId } = useContext(UserContext);
 
-  const newAccountHandler = useCallback(() => {
-    setIsNewAccount(true);
-    setModalVisible(true);
-  }, [setIsNewAccount, setModalVisible]);
+  const handleLogin = () => {
+    // TODO: Add authentication logic here
+    // For now, we just navigate to the next screen
+    navigation.navigate('Pantry');
+  };
 
-  const openLoginModal = useCallback(() => {
-    setModalVisible(true);
-  }, [setModalVisible]);
+  const handleSignUp = () => {
+    // TODO: Handle sign-up logic or navigation here
+    navigation.navigate('createaccountpage')
+  };
 
-  const closeLoginModal = useCallback(() => {
-    setModalVisible(false);
-    setIsNewAccount(false);
-  }, [setModalVisible, setIsNewAccount]);
-
-  const sendLogin = useCallback(() => {
-    // TODO: rig up to API
-    setUserId(DEFAULT_USER_ID);
-  }, [setUserId]);
 
   return (
     <View style={styles.container}>
-      <Modal visible={modalVisible} animationType="slide" transparent={false}>
-        <Text style={styles.headModalText}>Kitchenwise.</Text>
-        <View style={styles.loginContainer}>
-          <View style={styles.inputContainer}>
-            <Input
-              style={styles.loginInput}
-              placeholder="Username"
-              onChangeText={(text) => setUsername(text)}
-            />
-            <Input
-              style={styles.loginInput}
-              placeholder="Password"
-              onChangeText={(text) => setPassword(text)}
-            />
-            {isNewAccount && (
-              <Input
-                style={styles.loginInput}
-                placeholder="Confirm Password"
-                onChangeText={(text) => setPassword(text)}
-              />
-            )}
-          </View>
-          <LoginButton
-            text="Login"
-            isBlack={true}
-            onPress={() => {
-              // TODO : Add Auth here later
-              sendLogin();
-              closeLoginModal();
-              navigation.navigate("Pantry");
-            }}
-          />
-          <LoginButton
-            text="Cancel"
-            onPress={closeLoginModal}
-            isBlack={false}
-          />
-        </View>
-      </Modal>
-      <CenterText />
-      <View style={styles.buttonContainer}>
-        <LoginButton text="Login" onPress={openLoginModal} isBlack={true} />
-        <LoginButton
-          text="Create New Account"
-          onPress={newAccountHandler}
-          isBlack={false}
+      <Text style={styles.header}>kitchenwise.</Text>
+      <Text style={styles.subtitle}>The Future of Cooking.{"\n"}Powered by Your Voice.</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email address"
+          value={email}
+          onChangeText={setEmail}
         />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          secureTextEntry
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+        </TouchableOpacity>
       </View>
-    </View>
-  );
-};
 
-const CenterText = () => {
-  // text at center of page befote log in button is pressed
-  return (
-    <View style={styles.textContainer}>
-      <Text style={styles.headText}>Kitchenwise.</Text>
-      <Text style={styles.bodyText}>
-        The Future of Cooking. Powered by Alexa.
-      </Text>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
+
+
+      <View style={styles.socialLoginContainer}>
+        <Text style={styles.orText}>Or sign in with</Text>
+        <View style={styles.socialButtons}>
+          <Image source={require('../../assets/google-icon.png')} style={styles.socialIcon} />
+        </View>
+      </View>
+      <View style={styles.signUpContainer}>
+        <Text style={styles.orText}>Or</Text>
+        <TouchableOpacity onPress={handleSignUp}>
+          <Text style={styles.signUpText}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    // main container holding everything
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  textContainer: {
-    // container for text in the center
-    flex: 9,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "75%",
+  header: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#64D2D9',
+    marginTop: '-25%',
   },
-  buttonContainer: {
-    // container for button
-    flex: 3,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "90%",
-    marginBottom: "50%",
-  },
-  headText: {
-    // header text
-    alignItems: "center",
-    justifyContent: "center",
-    // fontWeight: 600,
-    fontSize: 35,
-    padding: 20,
-  },
-  headModalText: {
-    // header text
-    flex: 1,
-    marginTop: "50%",
-    // fontWeight: 600,
-    fontSize: 35,
-    padding: 20,
-    marginBottom: 0,
-  },
-  bodyText: {
-    // text following header text
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 15,
-  },
-  loginContainer: {
-    flex: 4,
-    flexDirection: "column",
-    alignItems: "center",
-    width: "80%",
-    alignSelf: "center",
-  },
-  loginInput: {
-    padding: 8,
-    width: "100%",
-    borderColor: "#cccccc",
-    borderWidth: 1,
-    margin: 5,
-    color: "black",
-    borderRadius: 8,
+  subtitle: {
+    fontSize: 20,
+    color: '#353434',
+    marginBottom: '20%'
+
   },
   inputContainer: {
-    width: "100%",
-    marginBottom: 50,
+    width: '90%',
+    marginBottom: 16,
+  },
+  input: {
+    backgroundColor: '#f7f7f7',
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#e1e1e1',
+    borderRadius: 20,
+    padding: 16,
+    marginBottom: "5%",
+    fontSize: 18, // Updated size for larger placeholder text
+    textAlign: 'center', // Center text horizontally
+    textAlignVertical: 'center', // Center text vertically
+    height: 50, // Fixed height to ensure vertical centering
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  
+  },
+  forgotPasswordText: {
+    alignSelf: 'flex-end',
+    color: '#000',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: '#000',
+    paddingVertical: 12,
+    width: '90%',
+    borderRadius: 20,
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  socialLoginContainer: {
+    alignItems: 'center',
+    marginBottom: 16,
+    marginTop: 30,
+  },
+  orText: {
+    color: '#000',
+    marginBottom: 8,
+    fontSize: 16,
+  },
+  socialButtons: {
+    paddingTop: '2.5%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    // width: '%',
+  },
+  socialIcon: {
+    width: 35,
+    height: 35,
+  },
+  signUpContainer: {
+    position: 'absolute',
+    bottom: '5%',
+    alignItems: 'center',
+  },
+  signUpText: {
+    color: '#5B7B94',
+    fontWeight: 'bold',
+    fontSize: 20
   },
 });
 
